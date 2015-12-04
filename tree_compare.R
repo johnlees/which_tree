6249f03b7605dfefe273a752955dd74e7fcb0a36
John Lees <lees.john6@gmail.com>
2015-12-04 14:28:03 +0000

    Add mash; better supertree plot

--- tree_compare.R
+++ tree_compare.R
@@ -2,6 +2,8 @@ require(ape)
 require(phangorn)
 require(exploratree)
 
+setwd("~/Documents/PhD/which_tree/trees/")
+
 source("../code/baps_score.R")
 
 tree_locations <- "/Users/jl11/Documents/PhD/which_tree/trees"
@@ -34,6 +36,13 @@ tr_andi_nj <- midpoint(nj(andi.matrix))
 tr_andi_bionj <- midpoint(bionj(andi.matrix))
 tr_andi_upgma <-  midpoint(upgma(andi.matrix))
 
+mash.matrix <- as.matrix(read.table(paste(distance_locations,"mash_distances.txt",sep = "/"), quote="\"", comment.char=""))
+dimnames(mash.matrix) = list(samples, samples)
+
+tr_mash_nj <- midpoint(nj(mash.matrix))
+tr_mash_bionj <- midpoint(bionj(mash.matrix))
+tr_mash_upgma <-  midpoint(upgma(mash.matrix))
+
 ncd_distances <- as.matrix(read.delim(paste(distance_locations,"ncd_distances.txt",sep = "/"), header=FALSE))
 dimnames(ncd_distances) = list(samples,samples)
 
@@ -48,9 +57,9 @@ tr_kmer_nj <- midpoint(nj(kmer_distances))
 tr_kmer_bionj <- midpoint(bionj(kmer_distances/max(kmer_distances))) # distances must be < 100
 tr_kmer_upgma <- midpoint(upgma(kmer_distances))
 
-all_trees <- list(realtr, tr_parsnp, tr_fasttree_slow, tr_fasttree_fast, tr_snp_jc_nj, tr_snp_logdet_nj, tr_phyml_all, tr_raxml_cactus, tr_raxml_mlst, tr_raxml_core, tr_raxml_23F, tr_raxml_all, tr_raxml_snps, tr_andi_upgma, tr_andi_nj, tr_andi_bionj, tr_kmer_nj, tr_kmer_upgma, tr_kmer_bionj, tr_ncd_nj, tr_ncd_upgma, tr_ncd_bionj)
+all_trees <- list(realtr, tr_parsnp, tr_fasttree_slow, tr_fasttree_fast, tr_snp_jc_nj, tr_snp_logdet_nj, tr_phyml_all, tr_raxml_cactus, tr_raxml_mlst, tr_raxml_core, tr_raxml_23F, tr_raxml_all, tr_raxml_snps, tr_andi_upgma, tr_andi_nj, tr_andi_bionj, tr_mash_upgma, tr_mash_nj, tr_mash_bionj, tr_kmer_nj, tr_kmer_upgma, tr_kmer_bionj, tr_ncd_nj, tr_ncd_upgma, tr_ncd_bionj)
 class(all_trees) <- "multiPhylo"
-names(all_trees) = c("realtr", "tr_parsnp", "tr_fasttree_slow", "tr_fasttree_fast", "tr_snp_jc_nj", "tr_snp_logdet_nj", "tr_phyml_all", "tr_raxml_cactus", "tr_raxml_mlst", "tr_raxml_core", "tr_raxml_23F", "tr_raxml_all", "tr_raxml_snps", "tr_andi_upgma", "tr_andi_nj", "tr_andi_bionj", "tr_kmer_nj", "tr_kmer_upgma", "tr_kmer_bionj", "tr_ncd_nj", "tr_ncd_upgma", "tr_ncd_bionj")
+names(all_trees) = c("realtr", "tr_parsnp", "tr_fasttree_slow", "tr_fasttree_fast", "tr_snp_jc_nj", "tr_snp_logdet_nj", "tr_phyml_all", "tr_raxml_cactus", "tr_raxml_mlst", "tr_raxml_core", "tr_raxml_23F", "tr_raxml_all", "tr_raxml_snps", "tr_andi_upgma", "tr_andi_nj", "tr_andi_bionj", "tr_mash_upgma", "tr_mash_nj", "tr_mash_bionj", "tr_kmer_nj", "tr_kmer_upgma", "tr_kmer_bionj", "tr_ncd_nj", "tr_ncd_upgma", "tr_ncd_bionj")
 # non-NJ trees
 direct_trees <- list(realtr, tr_parsnp, tr_fasttree_slow, tr_fasttree_fast, tr_snp_jc_nj, tr_snp_logdet_nj, tr_phyml_all, tr_raxml_cactus, tr_raxml_mlst, tr_raxml_core, tr_raxml_23F, tr_raxml_all, tr_raxml_snps)
 class(direct_trees) <- "multiPhylo"
@@ -80,6 +89,11 @@ dimnames(topology_all) = list(names(all_trees), names(all_trees))
 plot(midpoint(nj(topology_all)))
 plot(cmdscale(topology_all))
 text(cmdscale(topology_all),names(all_trees), cex=0.6, pos=4, col="red")
+
+# Nice plot of supertree
+pdf(file="../tree_plots/supertree.pdf",width = 10, height = 8)
+plot(midpoint(nj(topology_all)),tip.color=c("red",rep("black",length(midpoint(nj(topology_all))$tip.label)-1)),font=1)
+dev.off()
 
 branch_direct <- as.matrix(multi.dist(direct_trees, lambda = 1))
 dimnames(branch_direct) = list(names(direct_trees), names(direct_trees))
